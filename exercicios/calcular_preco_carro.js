e86a61fe7a544f6e86367f763e29fa9b2d338a31
glesiosantos <glesioss@gmail.com>
2023-11-09 17:23:38 -0300

    calcular preco

---
+++ exercicios/calcular_preco_carro.js
@@ -0,0 +1,7 @@
+var preco, valorVenda, margemLucro
+
+preco = 56000
+margemLucro = 0.20
+valorVenda = preco + (preco * margemLucro) + (preco * 0.28) + (preco * 0.45)
+
+console.log(`Valor do carro será de R$ ${valorVenda}`)
